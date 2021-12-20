34bad45a34274084f98dc060cc86cbda21da40d4
bdnaimur <webnaimur@gmail.com>
2021-12-20 20:22:38 +0600

    some coded added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
 const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -9,11 +11,28 @@ dotenv.config();
 const { Schema } = mongoose;
 
 const blogSchema = new Schema({
-    title: String, // String is shorthand for {type: String}
-    author: String,
+    name: {
+        type: String,
+        required: true,
+      },
+      username: {
+          type: String,
+          required: true,
+      },
+      password: {
+          type: String,
+          required: true,
+      },
+
+      token: String,
+
+      status: {
+        type: String,
+        enum: ["active", "inactive"],
+      }
 });
 
-const port = 3000
+const port = 3500
 const dbURL = 'ongodb+srv://mongoose_backend:naimur88@cluster0.esvfp.mongodb.net/crud?retryWrites=true&w=majority'
 
 mongoose.connect(process.env.Mongoose_URL,
@@ -24,8 +43,33 @@ mongoose.connect(process.env.Mongoose_URL,
     .then(console.log('connect MongoDb'))
     .catch(err => console.log('Mongo err', err))
 
-const Blog = mongoose.model('Blog', blogSchema);
+const Blog = mongoose.model('User', blogSchema);
 console.log(Blog);
+
+app.post('/user/signup', async function (req, res) {
+    const post = req.body;
+    console.log(post);
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const token = await jwt.sign({
+        username:req.body.username,
+        name: req.body.name,
+    }, process.env.JWT_SECRET, {
+        expiresIn: '1h'
+    });
+    console.log(token);
+    const createdPost = await Blog.create({
+        name: req.body.name,
+        username: req.body.username,
+        password: hashedPassword,
+        token: token
+    });
+    return res.status(201).json(createdPost);
+  })
+
+app.post('/user/login', async (req, res) =>{
+    const user = await Blog.find({username: req.body.username})
+    console.log(user);
+})
 
 app.get('/', (req, res) => {
     res.send('Welcome!')
